ae6dcf12b76b028bd73e42efd710386f865a1f61
Nicholas Davis <nickdavis1018@gmail.com>
2021-09-11 11:47:18 -0700

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/greeting/:name', (req, res)=>{
     res.send(`What's up, ${req.params.name}!`);
   });
 
+app.get('/tip/:total/:tipPercentage', (req, res)=>{
+    res.send('Tip should be: ' + `${req.params.total *= (.01 * req.params.tipPercentage)}`);
+  }); 
+
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`);
   });
